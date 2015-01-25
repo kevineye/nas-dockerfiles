b39289c01762e2e3d0b100a0f79d164943c486ef
Kevin Eye <kevineye@gmail.com>
2015-01-25 14:33:25 -0500

    fixed time machine; added avahi as separate container

--- timemachine-32/README.md
+++
@@ -1,5 +0,0 @@
-docker build -t kevineye/timemachine-32 timemachine-32
-
-docker run --name timemachine -d -p 548:548 -v ~/Desktop/TimeMachine:/TimeMachine -e AFPD_LOGIN=eye -e AFPD_PASSWORD=___ -e AFPD_NAME="Time Machine" -e AFPD_SIZE_LIMIT=1000 kevineye/timemachine-32
-
-http://192.168.59.103:32400/web
--- timemachine-32/run.sh
+++ timemachine-32/run.sh
@@ -24,14 +24,14 @@ if [ ! -e /.initialized ]; then
     echo "- -tcp -noddp -uamlist uams_randnum.so,uams_dhx.so,uams_dhx2.so -nosavepassword -setuplog \"default log_info /dev/stdout\"" > /etc/netatalk/afpd.conf
 
     if [ -z $AFPD_SIZE_LIMIT ]; then
-        echo "/TimeMachine \"${AFPD_NAME}\" allow:$AFPD_LOGIN rwlist:$AFPD_LOGIN fperm:664 dperm:775 cnidscheme:dbd options:usedots,uperm,tm" > /etc/netatalk/AppleVolumes.default
+        echo "/share \"${AFPD_NAME}\" allow:$AFPD_LOGIN rwlist:$AFPD_LOGIN fperm:664 dperm:775 cnidscheme:dbd options:usedots,uperm,tm" > /etc/netatalk/AppleVolumes.default
     else
-        echo "/TimeMachine \"${AFPD_NAME}\" allow:$AFPD_LOGIN rwlist:$AFPD_LOGIN fperm:664 dperm:775 cnidscheme:dbd options:usedots,uperm,tm volsizelimit:$AFPD_SIZE_LIMIT" > /etc/netatalk/AppleVolumes.default
+        echo "/share \"${AFPD_NAME}\" allow:$AFPD_LOGIN rwlist:$AFPD_LOGIN fperm:664 dperm:775 cnidscheme:dbd options:usedots,uperm,tm volsizelimit:$AFPD_SIZE_LIMIT" > /etc/netatalk/AppleVolumes.default
     fi
 
     touch /.initialized
 fi
 
-chown $AFPD_LOGIN /TimeMachine
+chown $AFPD_LOGIN /share
 
 exec ionice -c 3 /usr/bin/supervisord -n
