22b55961622a2ffa4b93182b30c09e74f73317ac
Kevin Eye <kevineye@gmail.com>
2015-05-30 13:43:13 -0400

    added makefile; updated for data-user

--- utorrent-32/run.sh
+++ utorrent-32/run.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-sed -i -e "s/:1500:1500:/:$UT_UID:$UT_GID:/" /etc/passwd
-sed -i -e "s/utorrent:x:1500:/utorrent:x:$UT_GID:/" /etc/group
-
-chown ${UT_UID}:${UT_GID} /share /opt/utorrent-server
-
-exec su utorrent -c "umask $UT_UMASK; /opt/utorrent-server/utserver -settingspath /opt/utorrent-server -logfile /dev/stdout"
+umask 02
+/opt/utorrent-server/utserver -settingspath /opt/utorrent-server -logfile /dev/stdout
