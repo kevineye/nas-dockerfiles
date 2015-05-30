36472b9c52e5f2589cc4da7fda7a83106b7fdfa1
Kevin Eye <kevineye@gmail.com>
2015-05-30 09:15:01 -0400

    using data-user, PUBLIC_HTTP_PORT

--- transmission-32/run.sh
+++ transmission-32/run.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-sed -i -e "s/:1500:1500:/:$TRANSMISSION_UID:$TRANSMISSION_GID:/" /etc/passwd
-sed -i -e "s/transmission:x:1500:/transmission:x:$TRANSMISSION_GID:/" /etc/group
-
 CONFIG_DIR=/etc/transmission-daemon
 SETTINGS=$CONFIG_DIR/settings.json
-TRANSMISSION=/usr/bin/transmission-daemon
 
 if [ ! -f ${SETTINGS}.bak ]; then
     if [ -z $ADMIN_PASS ]; then
@@ -16,6 +12,4 @@ if [ ! -f ${SETTINGS}.bak ]; then
     sed -i.bak -e "s/@password@/$ADMIN_PASS/" $SETTINGS 
 fi
 
-chown ${TRANSMISSION_UID}:${TRANSMISSION_GID} /var/lib/transmission-daemon /etc/transmission-daemon
-
-exec su transmission -c "umask $TRANSMISSION_UMASK; $TRANSMISSION -f --no-portmap --config-dir $CONFIG_DIR --log-info"
+/usr/bin/transmission-daemon -f --no-portmap --config-dir $CONFIG_DIR --log-info
